cfc922260a8bafd749ea87d3db2eb9e729077c2d
Bahur14 <149169826+Bahur14@users.noreply.github.com>
2024-05-23 13:17:25 +0300

    Delete Lab1s directory

--- Lab1s/mvc/model/profilemodel.js
+++
@@ -1,9 +0,0 @@
-const Model = {
-    getProfileData: function() {
-      const profileData = JSON.parse(localStorage.getItem('profileData'));
-      return profileData ? profileData : {};
-    },
-    setProfileData: function(data) {
-      localStorage.setItem('profileData', JSON.stringify(data));
-    }
-  };
